d7d7254efa18f476f205fd87870900c35b397c28
Yura Korchagin <yura.korchagin.03@gmail.com>
2024-04-04 18:45:12 +0300

    +overlay

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 
 const menuBtn = document.getElementById('menuBtn');
 const headerNav = document.querySelector('.header__nav');
+const overlayEl = document.querySelector('.overlay');
 
 menuBtn.addEventListener('click', () => {
     headerNav.classList.toggle('active');
+    overlayEl.classList.toggle('block')
 })
