31fdd9528c1d4c2db7c17c1b055a78088ed943b7
Serega <aleksei87-2010@mail.ru>
2024-04-08 15:36:14 +0300

    test fixed miner button (IOS)

--- js/miner.js
+++ js/miner.js
@@ -5,21 +5,28 @@ const minerBtn = document.getElementById("miner");
 const minerGame = document.querySelector('.miner');
 const minerCoin = document.getElementById('minerBtn');
 
-minerCoin.addEventListener('click', () => {
-    scoreCounter.dataset.score++;
-    scoreCounter.textContent = scoreCounter.dataset.score;
-})
+const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 
-minerBtn.addEventListener('click', (ev) => {
+const handleClick = () => {
     if (labirinthGame.classList.contains('game-block') || paintGame.classList.contains('game-block')) {
         labirinthGame.classList.remove('game-block');
         paintGame.classList.remove('game-block');
     }
     minerGame.classList.add('game-block');
+};
+
+if (isIOS) {
+    minerBtn.addEventListener('touchstart', handleClick);
+} else {
+    minerBtn.addEventListener('click', handleClick);
+}
+
+minerCoin.addEventListener('click', () => {
+    scoreCounter.dataset.score++;
+    scoreCounter.textContent = scoreCounter.dataset.score;
 });
 
 document.addEventListener('DOMContentLoaded', () => {
     minerGame.classList.add('game-block')
-})
-
+});
 
