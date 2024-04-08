7425436d1385dc1135ba2d32e00a9e4a48158391
Serega <aleksei87-2010@mail.ru>
2024-04-08 20:25:35 +0300

    fix toch games 2

--- js/miner.js
+++ js/miner.js
@@ -1,32 +1,38 @@
 "use strict";
-
-
 const minerBtn = document.getElementById("miner");
 const minerGame = document.querySelector('.miner');
 const minerCoin = document.getElementById('minerBtn');
 
-const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-
-const handleClick = () => {
-    if (labirinthGame.classList.contains('game-block') || paintGame.classList.contains('game-block')) {
-        labirinthGame.classList.remove('game-block');
-        paintGame.classList.remove('game-block');
+minerCoin.addEventListener('click', (event) => {
+    
+    if (event.target === minerCoin) {
+        scoreCounter.dataset.score++;
+        scoreCounter.textContent = scoreCounter.dataset.score;
     }
-    minerGame.classList.add('game-block');
-};
-
-if (isIOS) {
-    minerBtn.addEventListener('touchstart', handleClick);
-} else {
-    minerBtn.addEventListener('click', handleClick);
-}
-
-minerCoin.addEventListener('click', () => {
-    scoreCounter.dataset.score++;
-    scoreCounter.textContent = scoreCounter.dataset.score;
 });
+
+
+if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+    minerBtn.addEventListener('touchstart', (event) => {
+       
+        if (event.target === minerBtn) {
+            if (labirinthGame.classList.contains('game-block') || paintGame.classList.contains('game-block')) {
+                labirinthGame.classList.remove('game-block');
+                paintGame.classList.remove('game-block');
+            }
+            minerGame.classList.add('game-block');
+        }
+    });
+} else {
+    minerBtn.addEventListener('click', () => {
+        if (labirinthGame.classList.contains('game-block') || paintGame.classList.contains('game-block')) {
+            labirinthGame.classList.remove('game-block');
+            paintGame.classList.remove('game-block');
+        }
+        minerGame.classList.add('game-block');
+    });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     minerGame.classList.add('game-block')
 });
-
